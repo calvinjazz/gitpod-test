ce7e326d20dcf59c28ef335797f1b5f52f664f0b
calvinjazz <calvinjazz.0330@gmail.com>
2022-03-15 23:13:27 +0000

    configuring mysqldb

--- src/app.py
+++ src/app.py
@@ -27,6 +27,12 @@ def delete_todo(position):
     jsonified = jsonify(todos)
     return jsonified
 
+@app.route('/contacts', methods=['GET'])
+def hello_world():
+    jsonified = jsonify(todos)
+
+    return jsonified
+
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
